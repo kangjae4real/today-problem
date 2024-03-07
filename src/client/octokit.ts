0c9546e2503608e2f66083cff718c12b0657b2ec
Kangjae Choi <awpe89@gmail.com>
2024-03-07 13:59:08 +0900

    Add Comment

--- src/client/octokit.ts
+++ src/client/octokit.ts
@@ -10,6 +10,12 @@ export type RequestParams<RouteKey> = RouteKey extends keyof Endpoints
   ? Endpoints[RouteKey]["parameters"] & RequestParameters
   : RequestParameters;
 
+/*
+ * @kangjae4real
+ * NOTE: Octokit Client를 다시 Wrapping하는 이유.
+ * Octokit Instance의 reqeust 메소드는 Parameter와 그 Return value의 Type Hinting이 되지 않습니다.
+ * 사용의 불편함이 있어 accept Parameter를 포함하여 Wrapping하고 확장하여 사용합니다.
+ */
 export const request = <RouteKey extends keyof Endpoints>(
   route: RouteKey,
   params: RequestParams<RouteKey>,
